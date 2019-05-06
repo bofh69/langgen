f98660a12c8d870c22b1d6d1cd69654d818ae3a4
Sebastian Andersson <sebastian@bittr.nu>
2019-05-06 22:02:41 +0200

    Move template code to its own module

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod named;
 mod suffix;
+pub mod templates;
 
 /// The gender of Named:s.
 #[derive(Copy, Clone, Debug)]
@@ -59,34 +60,6 @@ pub trait Output: Viewer {
     fn write_style(&mut self, style: &str);
     fn done(&mut self);
     fn out(&mut self) -> OutputBuilder;
-}
-
-/**
- * Objects are used in templates for obj, env etc.
- */
-pub enum ObjectRef<'a> {
-    // Object(&'a Object),
-    // CountedObject(i64, &'a Object),
-    Int(i64),
-    String(&'a str),
-}
-
-/**
- * Context contains the objects referenced in templates.
- */
-pub trait Context {
-    fn get_me(&self) -> ObjectRef; // The subject.
-    fn get_obj(&self) -> ObjectRef; // The object.
-    fn get_env(&self) -> ObjectRef; // Another object.
-
-    fn get(&self, who: &str) -> ObjectRef; // ie obj2, num, str
-}
-
-/**
- * Template contains the text's to generate a text.
- */
-pub trait Template {
-    fn render(&self, ctx: &Context, out: &Output);
 }
 
 fn last_char(s: &str) -> Option<char> {
---
+++ src/templates.rs
@@ -0,0 +1,39 @@
+use crate::{Object, Output};
+
+/**
+ * Objects are used in templates for obj, env etc.
+ */
+pub enum ObjectRef<'a> {
+    Object(&'a Object),
+    CountedObject(i64, &'a Object),
+    Int(i64),
+    String(&'a str),
+}
+
+/**
+ * Context contains the objects referenced in templates.
+ */
+pub trait Context {
+    fn get_me(&self) -> ObjectRef; // The subject.
+    fn get_obj(&self) -> ObjectRef; // The object.
+    fn get_env(&self) -> ObjectRef; // Another object.
+
+    fn get(&self, who: &str) -> ObjectRef; // ie obj2, num, str
+}
+
+/**
+ * Template contains the text's to generate a text.
+ */
+pub trait Template {
+    fn render(&self, ctx: &Context, out: &Output);
+}
+
+    /*
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_parse_template() {
+    }
+}
+    */
